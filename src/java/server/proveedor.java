8c2d8cba0e5c6cf7f1e1f34958413cba57647cd8
Ismael1 <Ismael1@Ismael>
2019-03-23 13:12:59 +0000

    Servidor Proveedores

---
+++ src/java/server/proveedor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Ismael1
+ */
+@WebService(serviceName = "proveedor")
+public class proveedor {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "comprar")
+    public Boolean comprar(@WebParam(name = "isbn") String isbn, @WebParam(name = "cantidad") int cantidad) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "novedades")
+    public String novedades() {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "ofertas")
+    public String ofertas() {
+        //TODO write your implementation code here:
+        return null;
+    }
+    
+    
+}
